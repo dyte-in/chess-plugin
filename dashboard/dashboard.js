3fd1bf629238c8eaf213034ee1afb2ef268457d5
Rohan Mukherjee <rohanbayern@gmail.com>
2021-01-17 22:38:37 +0530

    fix: Linting issues

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,51 +1,44 @@
-
 (function () {
-      WinJS.UI.processAll().then(function () {
+      WinJS.UI.processAll().then(() => {
+            const socket = io();
+            const games = {};
+            const boards = {};
 
-            var socket = io();
-            var games = {};
-            var boards = {};
-
-
-            //////////////////////////////
+            /// ///////////////////////////
             // Socket.io handlers
-            ////////////////////////////// 
+            /// ///////////////////////////
 
             socket.emit('dashboardlogin');
-            socket.on('dashboardlogin', function (msg) {
+            socket.on('dashboardlogin', (msg) => {
                   createGamesList(msg.games);
             });
 
-
-            socket.on('gameadd', function (msg) {
-                  initGame(msg.gameId, msg.gameState)
+            socket.on('gameadd', (msg) => {
+                  initGame(msg.gameId, msg.gameState);
             });
 
-            socket.on('resign', function (msg) {
-                  var gameToRemove = document.getElementById('game-board' + msg.gameId);
+            socket.on('resign', (msg) => {
+                  const gameToRemove = document.getElementById(`game-board${msg.gameId}`);
                   gameToRemove.parentElement.removeChild(gameToRemove);
             });
 
-            socket.on('move', function (msg) {
+            socket.on('move', (msg) => {
                   games[msg.gameId].move(msg.move);
                   boards[msg.gameId].position(games[msg.gameId].fen());
-
             });
 
-
-            //////////////////////////////
+            /// ///////////////////////////
             // Chess Games
-            ////////////////////////////// 
+            /// ///////////////////////////
 
             var createGamesList = function (serverGames) {
-                  Object.keys(serverGames).forEach(function (gameId) {
+                  Object.keys(serverGames).forEach((gameId) => {
                         initGame(gameId, serverGames[gameId]);
                   });
             };
 
             var initGame = function (gameId, serverGame) {
-
-                  var cfg = {
+                  const cfg = {
                         draggable: false,
                         showNotation: false,
                         orientation: 'white',
@@ -53,16 +46,15 @@
                         position: serverGame.board ? serverGame.board : 'start',
                   };
 
-                  // create the game parent div 
-                  $('#games').append($('<div id="game-board' + gameId + '" class="gameboard"></div>'));
+                  // create the game parent div
+                  $('#games').append($(`<div id="game-board${gameId}" class="gameboard"></div>`));
 
-                  // create the game     
-                  var game = serverGame.board ? new Chess(serverGame.board) : new Chess();
+                  // create the game
+                  const game = serverGame.board ? new Chess(serverGame.board) : new Chess();
                   games[gameId] = game;
 
-                  var board = new ChessBoard('game-board' + gameId, cfg);
+                  const board = new ChessBoard(`game-board${gameId}`, cfg);
                   boards[gameId] = board;
-            }
+            };
       });
-})();
-
+}());
